1f35a48b7a090864b5ba9207d524d19f469d8053
nandmoh-2506 <askevamarketing@gmail.com>
2023-10-13 16:17:43 +0530

    Uma code updated

--- src/gitoctsam1/org/GitSam1.java
+++ src/gitoctsam1/org/GitSam1.java
@@ -3,7 +3,7 @@ package gitoctsam1.org;
 public class GitSam1 {
 	public static void main(String[] args) {
 
-		System.out.println("Uma coding is done");
+		System.out.println("Uma code");
 
 		System.out.println("Welcome to github");
 
